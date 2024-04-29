a364a499a6919eacb0168e4206ecf8c0f6a6b372
TiJayTJ <nurhantahaviev@gmail.com>
2024-04-30 00:57:29 +0300

    Добавьте файлы проекта.

---
+++ Atm.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Text;
+
+namespace ATM_machine
+{
+    class Atm(List<int> banknotesPile)
+    {
+        private readonly List<int> banknotes = [.. banknotesPile.OrderBy(x => x)];
+        private readonly List<List<int>> result = [];
+
+        public List<List<int>> Change(int nominal) 
+        { 
+            SolveRecursive(banknotes, nominal, []); 
+            return result;
+        }
+
+        private void SolveRecursive(in List<int> banknotes, in int nominal, List<int> partial)
+        {
+            int s = 0;
+            foreach (int x in partial) { s += x; }
+            if (s == nominal) { result.Add(partial); }
+            if (s >= nominal) { return; }
+
+            for (int i = 0; i < banknotes.Count; i++) 
+            {
+                var remaining = new List<int>();
+                int n = banknotes[i];
+                for (int j = i; j < banknotes.Count; j++) 
+                { 
+                    remaining.Add(banknotes[j]); 
+                }
+                var partialRec = new List<int>(partial) { n };
+
+                SolveRecursive(remaining, nominal, partialRec);
+            }
+        }
+
+        public static void PrintResult(in List<List<int>>result)
+        {
+            var bld = new StringBuilder();
+
+            foreach (List<int> exchange in result) 
+            {
+                foreach (int banknote in exchange)
+                {
+                    bld.Append(banknote + " ");
+                }
+                bld.Append('\n');
+            }
+            Console.WriteLine(bld.ToString());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using ATM_machine;
+
+class Program
+{
+    static void Main()
+    {
+        int nominal = GetNominal();
+        Console.WriteLine();
+        var banknotes = GetBanknotes();
+        var atm = new Atm(banknotes);
+        Console.WriteLine("Всевозможные варианты размена:");
+        Atm.PrintResult(atm.Change(nominal));
+    }
+
+    static private int GetNominal()
+    {
+        int result;
+        while (true)
+        {
+            Console.Write("Введите сумму(целое число), которую хотите разменять: ");
+            string? input = Console.ReadLine();
+
+            if (int.TryParse(input, out result))
+            {
+                if (result < 0)
+                {
+                    Console.WriteLine("Ошибка: Выдаваемая сумма не может быть отрицательной");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: Введено не целое число\n"); 
+            }
+        }
+        return result;
+    }
+    static List<int> GetBanknotes()
+    {
+        var banknotes = new List<int>();
+        string[] inputLine;
+        var continueCycle = true;
+
+        while (continueCycle)
+        {
+            Console.WriteLine("Введите доступные для размена купюры (целые числа) через пробел. " +
+                "\nЧтобы закончить ввод, введите 0.");
+            inputLine = Console.ReadLine().Split([' ']);
+            foreach (string input in inputLine) 
+            {
+                if (input == "0")
+                {
+                    continueCycle = false;
+                    break;
+                }
+
+                if (int.TryParse(input, out int number))
+                {
+                    if (number < 0)
+                    {
+                        Console.WriteLine("Ошибка: Купюра не может быть отрицательной.\n");
+                    }
+                    else
+                    {
+                        banknotes.Add(number);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: Некорректный ввод. Пожалуйста, введите целое число.\n");
+                }
+            }
+        }
+
+        return banknotes;
+    }
+}
